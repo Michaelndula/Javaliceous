a8153879a28b18596f84e48d24246b8b7e4a5087
Michael Ndula <michaelndula@gmail.com>
2023-07-07 21:53:36 +0300

    Variables in Java

--- iterate/src/main/java/org/example/Main.java
+++ iterate/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         Stream.iterate(1, i -> i + 1)
-                .limit(10)
+                .limit(200)
                 .forEach(System.out::println);
     }
 }
---
+++ variables/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//You are given code which outputs the properties of a vehicle, but something is wrong.
+//
+//        Task
+//        Fix the code to print the properties.
+//
+//        Output
+//        Name: Toyota
+//        Engine: 4.7
+//        Year: 2019
+public class Main {
+    public static void main(String[] args){
+
+        String model = "Toyota";
+        double engine = 4.7;
+        int year = 2019;
+
+        System.out.println("Name:" + model);
+        System.out.println("Engine:" + engine);
+        System.out.println("Year:" + year);
+    }
+}

a1b1bf436d94ebe6990803802ae762a5103ae8a0
Michael Ndula <michaelndula@gmail.com>
2023-07-09 22:21:14 +0300

    Sort Array in ascending order

--- var-prac/src/main/java/org/example/Main.java
+++ var-prac/src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
 
 public class Main {
-    static int Arr[] = {78, 34, 1, 3, 90, 34, -1, -4, 6, 55, 20, -65};
+    static int Arr[] = {78, 34, 1, 3, 90, 34, -1, -4, 6, 144, 55, 20, -65};
 
-    static int ascend() {
+    static void ascend() {
 
         for(int i =0; i < Arr.length; i++){
             for(int j = i +1; j < Arr.length; j++){
@@ -17,11 +17,10 @@ public class Main {
             }
             System.out.println(Arr[i]);
         }
-        return 0;
     }
 
     public  static void main(String[] args){
         System.out.println("Array after sorted: ");
-        System.out.println(ascend());
+        ascend();
     }
 }

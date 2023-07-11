2216b8d5da52d8b6976b49914f4b1722f9c0cb17
Michael Ndula <michaelndula@gmail.com>
2023-07-11 20:53:40 +0300

    conditions

---
+++ conditions/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 14;
+        if (age >= 18) {
+            System.out.println("You are ready!!");
+        }else{
+            System.out.println("You are not ready!!");
+        }
+    }
+}

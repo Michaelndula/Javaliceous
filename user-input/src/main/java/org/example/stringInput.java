a654406f32e2437d16572b90058b84e749fc1fc4
Michael Ndula <michaelndula@gmail.com>
2023-07-11 20:15:43 +0300

    Taking user input as a integer using the sc.nextInt function

--- user-input/src/main/java/org/example/stringInput.java
+++ user-input/src/main/java/org/example/stringInput.java
@@ -5,7 +5,8 @@ public class stringInput {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
+        int age = sc.nextInt();
 
-        System.out.println(name);
+        System.out.println(name +" is "+ age +" years old");
     }
 }

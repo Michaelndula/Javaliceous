33885c94ebde37a112cbd30d5b52c6c916b920c9
Michael Ndula <michaelndula@gmail.com>
2023-07-17 21:52:06 +0300

    If Statement

---
+++ control_statements/src/main/java/decision_making/if_statement.java
@@ -0,0 +1,11 @@
+package decision_making;
+
+public class if_statement {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 12;
+        if(x+y > 20) {
+            System.out.println("x + y is greater than 20");
+        }
+    }
+}

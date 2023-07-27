b91f3873a507281b79c7913fa5597e05edfbd1ab
Michael Ndula <michaelndula@gmail.com>
2023-07-27 21:29:07 +0300

    Default constructors in JAva

---
+++ constructor/src/main/java/default_constructor.java
@@ -0,0 +1,8 @@
+public class default_constructor {
+    default_constructor(){System.out.println("Consturctor was called");}
+
+    public static void main(String[] args){
+        default_constructor c1 = new default_constructor();
+    }
+
+}

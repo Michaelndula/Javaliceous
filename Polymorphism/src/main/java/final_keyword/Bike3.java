3f3df4960f63f579bfead4cc6aea76f267e5b055
Michael Ndula <michaelndula@gmail.com>
2023-08-04 06:38:43 +0300

    static binding

--- Polymorphism/src/main/java/final_keyword/Bike3.java
+++ Polymorphism/src/main/java/final_keyword/Bike3.java
@@ -1,13 +1,13 @@
-package final_keyword;
-
-final class Bike3 {
-}
-
-class Honda1 extends Bike3{
-    void run(){System.out.println("running safely");}
-
-    public static void main(String[] args){
-        Honda1 honda = new Honda1();
-        honda.run();
-    }
-}
+//package final_keyword;
+//
+//final class Bike3 {
+//}
+//
+//class Honda1 extends Bike3{
+//    void run(){System.out.println("running safely");}
+//
+//    public static void main(String[] args){
+//        Honda1 honda = new Honda1();
+//        honda.run();
+//    }
+//}

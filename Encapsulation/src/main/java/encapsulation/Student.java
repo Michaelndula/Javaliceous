1f5ac32f964874c20c1ed9194fd7e456afb0157f
Michael Ndula <michaelndula@gmail.com>
2023-08-07 06:52:29 +0300

    Encapsulation

---
+++ Encapsulation/src/main/java/encapsulation/Student.java
@@ -0,0 +1,15 @@
+package encapsulation;
+
+public class Student {
+    private String name;
+
+    //getter method
+    public String getName(){
+        return name;
+    }
+
+    //setter method
+    public void setName(String name){
+        this.name = name;
+    }
+}
---
+++ Encapsulation/src/main/java/encapsulation/Test.java
@@ -0,0 +1,11 @@
+package encapsulation;
+
+class Test {
+    public static void main(String[] args){
+        Student s = new Student();
+
+        s.setName("Michael");
+
+        System.out.println(s.getName());
+    }
+}
